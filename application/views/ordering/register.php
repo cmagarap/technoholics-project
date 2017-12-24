429d2eca30bbf847a3dad18427f6160079b46ce6
seej11010 <chrisjohn.seej@gmail.com>
2017-12-24 18:47:45 +0800

    customer UI update: register

--- application/views/ordering/register.php
+++ application/views/ordering/register.php
@@ -12,7 +12,9 @@
             <div class="col-md-6">
                 <div class="box">
                     <h1>New account</h1>
-                    <p class="lead">Not our registered customer yet?</p>
+                    <p class="lead" style = "display: inline">Not our registered customer yet? </p>
+                    <p class="text-muted" style = "display: inline"> Sign Up now.</p>
+                    <br><br>
                     <p>With registration with us new world of fashion, fantastic discounts and much more opens to you! The whole process will not take you more than a minute!</p>
                     <p class="text-muted">If you have any questions, please feel free to <a href="<?= base_url().'home/contact'; ?>">contact us</a>, our customer service center is working for you 24/7.</p>
                     <hr>
