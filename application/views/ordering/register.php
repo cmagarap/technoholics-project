dd1280d01c5b01b054cac6c1854fbe4cce647a20
seej11010 <chrisjohn.seej@gmail.com>
2017-12-24 18:34:57 +0800

    customer UI update: login

--- application/views/ordering/register.php
+++ application/views/ordering/register.php
@@ -1,28 +1,21 @@
 <div id="all">
-
     <div id="content">
         <div class="container">
-
             <div class="col-md-12">
-
                 <ul class="breadcrumb">
                     <li><a href="#">Home</a>
                     </li>
                     <li>New account / Sign in</li>
                 </ul>
-
             </div>
 
             <div class="col-md-6">
                 <div class="box">
                     <h1>New account</h1>
-
                     <p class="lead">Not our registered customer yet?</p>
                     <p>With registration with us new world of fashion, fantastic discounts and much more opens to you! The whole process will not take you more than a minute!</p>
                     <p class="text-muted">If you have any questions, please feel free to <a href="<?= base_url().'home/contact'; ?>">contact us</a>, our customer service center is working for you 24/7.</p>
-
                     <hr>
-
                     <form action="<?= base_url().'home/customer_orders'; ?>" method="post">
                         <div class="form-group">
                             <label for="name">Name</label>
@@ -42,17 +35,12 @@
                     </form>
                 </div>
             </div>
-
             <div class="col-md-6">
                 <div class="box">
                     <h1>Login</h1>
-
-                    <p class="lead">Already our customer?</p>
-                    <p class="text-muted">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies
-                        mi vitae est. Mauris placerat eleifend leo.</p>
-
+                    <p class="lead" style = "display: inline">Already our customer? </p>
+                    <p class="text-muted" style = "display: inline">Sign in now to start shopping.</p>
                     <hr>
-
                     <form action="<?= base_url().'home/customer_orders'; ?>" method="post">
                         <div class="form-group">
                             <label for="email">Email</label>
@@ -68,9 +56,5 @@
                     </form>
                 </div>
             </div>
-
-
-        </div>
-        <!-- /.container -->
-    </div>
-    <!-- /#content -->
+        </div> <!-- /.container -->
+    </div> <!-- /#content -->
