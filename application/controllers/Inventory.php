172a15ac4325c76a33f37d699daca36f88413ac3
BlueOptimist <veothadeus@hotmail.com>
2018-01-16 20:17:05 +0800

    Notification on add products

--- application/controllers/Inventory.php
+++ application/controllers/Inventory.php
@@ -91,6 +91,7 @@
 
         public function add_product_exec() {
             $this->form_validation->set_rules('supplier', "Please put the supplier company.", "required");
+            $this->form_validation->set_rules('product_brand', "Please put the product brand.", "required");
             $this->form_validation->set_rules('product_name', "Please put the product name.", "required");
             $this->form_validation->set_rules('product_price', "Please put the product price.", "required|numeric");
             $this->form_validation->set_rules('product_quantity', "Please put the product quantity.", "required|numeric");
@@ -98,7 +99,6 @@
             $this->form_validation->set_message('required', '{field}');
 
         if ($this->form_validation->run()) {
-
             $this->load->library('upload');
             $dataInfo = array();
             $files = $_FILES;
@@ -118,6 +118,7 @@
 
             $data = array(
                 'product_name' => trim($this->input->post('product_name')),
+                'product_brand' => $this->input->post('product_brand'),
                 'product_price' => $this->input->post('product_price'),
                 'product_quantity' => $this->input->post('product_quantity'),
                 'product_category' => $this->input->post('product_category'),
@@ -139,8 +140,13 @@
                 "action" => 'Added product: ' . trim($this->input->post('product_name')),
                 'status' => '1'
             );
-            $this->item_model->insertData('product', $data);
+
+            $insert = $this->item_model->insertData('product', $data);
             $this->item_model->insertData('user_log', $for_log);
+            $statusMsg = $insert? '<b>'.trim($this->input->post('product_name')).'</b>'.' has been added successfully.':'Some problem occured, please try again.';
+            $this->session->set_flashdata('statusMsg',$statusMsg);
+
+            redirect("inventory/page");
         }
 
         else {
